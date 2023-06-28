e3231ca536181d3759086ded0a8cb611cc7bde51
abalmasDA <abalmasit@gmail.com>
2023-06-28 21:06:30 +0300

    Commit1

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Employee {
+public class Employee {
+    private String name;
+    private int age;
+    private String position;
+
+
+    public Employee(String name, int age, String position) {
+        this.name = name;
+        this.age = age;
+        this.position = position;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", position='" + position + '\'' +
+                '}';
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class Main {
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Виведення на екран елементів List: Створити список, заповнити його на 10 елементів
+ * та вивести на екран вміст, використовуючи iterator.
+ */
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Employee> employeeList = new ArrayList<>() {
+            {
+                add(new Employee("Dmytro1", 25, "Structural Engineer"));
+                add(new Employee("Dmytro2", 30, "Structural Engineer"));
+                add(new Employee("Dmytro3", 20, "Structural Engineer"));
+                add(new Employee("Dmytro4", 21, "Structural Engineer"));
+                add(new Employee("Dmytro5", 48, "Structural Engineer"));
+                add(new Employee("Dmytro6", 44, "Structural Engineer"));
+                add(new Employee("Dmytro7", 57, "Structural Engineer"));
+                add(new Employee("Dmytro8", 18, "Structural Engineer"));
+                add(new Employee("Dmytro9", 19, "Structural Engineer"));
+                add(new Employee("Dmytro10", 29, "Structural Engineer"));
+            }
+
+        };
+
+        Iterator<Employee> listIterator = employeeList.listIterator();
+        while (listIterator.hasNext()) {
+            Employee employee = listIterator.next();
+            System.out.println(employee);
+
+        }
+
+    }
 }
